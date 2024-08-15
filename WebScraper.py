706d14ac8f523fe1719559f4ecaa982c5f7b03be
Hamudi <mofa2722@th-wildau.de>
2024-08-15 03:46:40 +0200

    2

--- WebScraper.py
+++ WebScraper.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-
 options = webdriver.ChromeOptions()
 
 options.add_experimental_option('prefs',
@@ -18,22 +17,20 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 URL = 'https://aisel.aisnet.org/wi2023'
 driver.get(URL)
 
-#tutorial zeigt folgendes
 driver.maximize_window()
 driver.implicitly_wait(3)
 
-# aus video geschrieben mit pfad aus WI seite
 cookie_div = driver.find_element(By.XPATH,'//div[@id="onetrust-button-group"]')
 if cookie_div: driver.find_element(By.XPATH, '//div[@id="onetrust-button-group"]//button[text()="Akzeptiere alle Cookies"]').click()
 
 try:
-    # Warte bis zu 10 Sekunden, bis ein bestimmtes Element auf der Seite geladen ist (passe den XPath entsprechend an)
+
     pdf_links = WebDriverWait(driver, 10).until(
         EC.presence_of_all_elements_located(
             (By.XPATH, "//a[contains(@href, 'viewcontent.cgi')]"))
     )
     for link in pdf_links:
-        link.click()  # Auf jeden Link klicken
+        link.click()
 
 
 except TimeoutException:
